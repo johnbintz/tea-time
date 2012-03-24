c654a753c524aaa6629b863c27ec8908ddedfc69
John Bintz <john@coswellproductions.com>
2012-03-24 10:20:56 -0400

    updates, time to do this

--- presentation.rb
+++ presentation.rb
@@ -3,5 +3,7 @@ require 'rack-livereload'
 Attentive.configure do |c|
   c.title = "Tea Time: A Beginner's Guide to Jasmine"
   c.middleware << Rack::LiveReload
+
+  #c.export_size = '1280x1024'
 end
 
